9f061d8bbdcc082d8df36eeef127490db9742b98
liusiyu579 <969529245@qq.com>
2019-10-24 20:59:21 +0800

    实现交换两个变量的值。要求：需要交换实参的值。

---
+++ 20191023/src/swap.java
@@ -0,0 +1,33 @@
+/**
+ * @ClassName swap
+ * @Description TODO
+ * @Author lenovo
+ * @Date 2019/10/23 21:03
+ * @Version 1.0
+ **/
+
+class Swap1{
+    private int a;
+    private int b;
+    public int getA() {
+        return a;
+    }
+    public void setA(int a) {
+        this.b = a;
+    }
+    public int getB() {
+        return b;
+    }
+    public void setB(int b) {
+        this.a = b;
+    }
+}
+public class swap {
+    public static void main(String[] args) {
+        Swap1 swp=new Swap1();
+        swp.setA(10);
+        swp.setB(20);
+        System.out.println("a= "+swp.getA());
+        System.out.println("b= "+swp.getB());
+    }
+}
