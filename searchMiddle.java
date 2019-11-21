8b2c973897c2d5432c33becdbdd1788d9eb2dbd8
liusiyu579 <969529245@qq.com>
2019-11-21 18:41:20 +0800

    给定一个整数类型的数组 nums，请编写一个能够返回数组“中心索引”的方法。、 数组中心索引的左侧所有元素相加的和等于右侧所有元素相加的和。

---
+++ searchMiddle.java
@@ -0,0 +1,19 @@
+
+//给定一个整数类型的数组 nums，请编写一个能够返回数组“中心索引”的方法。、
+//数组中心索引的左侧所有元素相加的和等于右侧所有元素相加的和。
+class searchMiddle {
+    public int pivotIndex(int[] nums) {
+        int sum=0, a=0;
+        for(int num:nums){
+            sum+=num;
+        }
+        for(int i=0;i<nums.length;i++){
+            if(sum-nums[i]==a*2){
+                return i;
+            }else {
+                a+=nums[i];
+            }
+        }
+        return -1;
+    }
+}
