feec8c5f8b0de70a5a3f48be6a1cd7f1333ae365
liusiyu579 <969529245@qq.com>
2019-12-11 18:59:35 +0800

    输入两个字符串 输出重复的字符

---
+++ 20191211/src/Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+/**
+ * @ClassName Main
+ * @Description TODO
+ * @Author lenovo
+ * @Date 2019/12/11 18:51
+ * @Version 1.0
+ **/
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String str1 = scan.nextLine();
+        String str2 = scan.nextLine();
+        StringBuilder sb = new StringBuilder();
+        for(int i=0;i<str1.length();i++){
+            char ch = str1.charAt(i);
+            if(str2.contains(ch+"")) {
+                sb.append(ch);
+            }
+        }
+        System.out.println(sb.toString());
+    }
+}
