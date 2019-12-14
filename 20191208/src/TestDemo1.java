7da9b70abbb3b9ec8e8a6745916e56eee814305f
liusiyu579 <969529245@qq.com>
2019-12-14 22:13:01 +0800

    输入一个字符串，重复不输出。

--- 20191208/src/TestDemo1.java
+++ 20191208/src/TestDemo1.java
@@ -13,6 +13,7 @@ public class TestDemo1 {
         Scanner scan = new Scanner(System.in);
         String str = scan.nextLine();
         StringBuilder sb = new StringBuilder();
+		
         for(int i = 0;i < str.length();i++) {
             char ch = str.charAt(i);
             if(!sb.toString().contains(ch + "") ) {
