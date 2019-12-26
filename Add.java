cac3e7b3857ee66f08d87da22cbc9f5305cc0393
liusiyu579 <969529245@qq.com>
2019-12-26 18:44:32 +0800

    整数加一

---
+++ Add.java
@@ -0,0 +1,17 @@
+
+//整数加一
+class Add {
+    public int[] plusOne(int[] digits) {
+        for(int i=digits.length-1;i>=0;i--){
+            if(digits[i]!=9){
+                digits[i]++;
+                return digits;
+            }else {
+                digits[i]=0;
+            }
+        }
+        int[] tmp=new int[digits.length+1];
+        tmp[0]=1;
+        return tmp;
+    }
+}
