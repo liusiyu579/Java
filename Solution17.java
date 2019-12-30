ea7e081e11f29875e8e35e696ce0a011f3464c12
liusiyu579 <969529245@qq.com>
2019-12-30 18:46:41 +0800

    将数组分成和相等的三个部分 可以将其划分为三个和相等的非空部分时才返回 true，否则返回 false。

---
+++ Solution17.java
@@ -0,0 +1,26 @@
+//将数组分成和相等的三个部分
+//可以将其划分为三个和相等的非空部分时才返回 true，否则返回 false。
+class Solution17 {
+    public boolean canThreePartsEqualSum(int[] A) {
+        int sum=0;;
+        for(int a:A){
+            sum+=a;
+        }
+        if(sum%3!=0) {
+            return false;
+        }
+        int b = 0;
+        int c = 0;
+        for(int d:A){
+            b+=d;
+            if(b != sum/3){
+                continue;
+            }
+            if(++c == 3) {
+                return true;
+            }
+            b = 0;
+        }
+        return false;
+    }
+}
