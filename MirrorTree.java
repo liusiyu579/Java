553913d88ca12570563296228eeca41c8b5b3ac2
liusiyu579 <969529245@qq.com>
2020-03-01 19:53:12 +0800

    操作给定的二叉树，将其变换为源二叉树的镜像。

---
+++ MirrorTree.java
@@ -0,0 +1,29 @@
+
+/**
+public class TreeNode {
+    int val = 0;
+    TreeNode left = null;
+    TreeNode right = null;
+
+    public TreeNode(int val) {
+        this.val = val;
+
+    }
+
+}
+*/
+public class MirrorTree {
+    public void Mirror(TreeNode root) {
+        if(root != null){
+            TreeNode tmp = root.left;
+            root.left = root.right;
+            root.right = tmp;
+            if(root.left != null){
+                Mirror(root.left);
+            }
+            if(root.right != null){
+                Mirror(root.right);
+            }
+        }
+    }
+}
