6468280b8be0975a513f1ee29d3659f390a33f76
liusiyu579 <969529245@qq.com>
2020-03-05 20:04:45 +0800

    根据一棵树的前序遍历与中序遍历构造二叉树。

---
+++ BuilderTree.java
@@ -0,0 +1,28 @@
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class BuilderTree {
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        if(preorder.length == 0 || inorder.length == 0){
+            return null;
+        }
+
+        TreeNode root = new TreeNode(preorder[0]);
+        for(int i = 0;i < preorder.length;i++){
+            if(preorder[0] == inorder[i]) {
+                root.left = buildTree(Arrays.copyOfRange(preorder,1,i+1),Arrays.copyOfRange(inorder,0,i));
+                root.right = buildTree(Arrays.copyOfRange(preorder,i+1,preorder.length),Arrays.copyOfRange(inorder,i+1,inorder.length));
+                break;
+            }
+        }
+        return root;
+    }
+}
