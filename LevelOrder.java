98b9c77b3cd04482d910bee519fa1fa626523508
liusiyu579 <969529245@qq.com>
2020-03-06 21:51:41 +0800

    给定一个二叉树，返回其按层次遍历的节点值。 （即逐层地，从左到右访问所有节点）。

---
+++ LevelOrder.java
@@ -0,0 +1,37 @@
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class LevelOrder {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> ret = new ArrayList<>();
+        if(root == null) {
+            return new ArrayList<>();
+        }
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+        while(!q.isEmpty()) {
+            int size = q.size();
+            List<Integer> level = new ArrayList<>();
+            for(int i = 0;i < size;i++) {
+                TreeNode node = q.poll();
+                level.add(node.val);
+                if(node.left != null) {
+                    q.offer(node.left);
+                }
+                if(node.right != null) {
+                    q.offer(node.right);
+                }
+            }
+            ret.add(level);
+        }
+        return ret;
+    }
+}
